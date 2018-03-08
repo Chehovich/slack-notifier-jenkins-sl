1a3e4bc7ec01e9d18474340e6bb96378ede17a03
Chehovich <Chehovich@users.noreply.github.com>
2018-03-08 16:21:07 +0200

    add custom message to notify result

--- src/org/gradiant/jenkins/slack/SlackNotifier.groovy
+++ src/org/gradiant/jenkins/slack/SlackNotifier.groovy
@@ -58,6 +58,37 @@ void notifyResult() {
   sender.send message, color
 }
 
+void notifyCustomResult(String custom) {
+  JenkinsHelper helper = new JenkinsHelper()
+  JenkinsStatus status = new JenkinsStatus()
+  SlackFormatter formatter = new SlackFormatter()
+  SlackSender sender = new SlackSender()
+  Config config = new Config()
+
+  def statusMessage = status.getStatusMessage()
+
+  if(shouldNotNotifySuccess(statusMessage)) {
+    println("SlackNotifier - No notification will be send for SUCCESS result")
+    return
+  }
+
+  def color = status.getStatusColor()
+  def duration = helper.getDuration()
+
+  String changes = null
+  if(config.getChangeList()) changes = helper.getChanges().join '\n'
+
+  String testSummary = null
+  if (config.getTestSummary()) {
+    JenkinsTestsSummary jenkinsTestsSummary = new JenkinsTestsSummary()
+    testSummary = jenkinsTestsSummary.getTestSummary()
+  }
+
+  def message = formatter.format "${statusMessage} after ${duration}", changes, testSummary, custom
+
+  sender.send message, color
+}
+
 void notifyResultFull() {
   env.TEST_SUMMARY = true
   env.CHANGE_LIST = true
