ee7ea51d13483254fc1781c2815a22cc86cb8230
Chehovich <Chehovich@users.noreply.github.com>
2018-03-08 16:30:04 +0200

    add artifact param

--- src/org/gradiant/jenkins/slack/SlackFormatter.groovy
+++ src/org/gradiant/jenkins/slack/SlackFormatter.groovy
@@ -15,3 +15,19 @@ String format(String title = '', String message = '', String testSummary = '') {
 
   return result
 }
+
+String format(String title = '', String message = '', String testSummary = '', String artifact = '') {
+  def helper = new JenkinsHelper()
+
+  def project = helper.getProjectName()
+  def branch = helper.getBranchName()
+  def buildNumber = helper.getBuildNumber()
+  def url = helper.getAbsoluteUrl()
+
+  def result = "${project} » ${branch} - #${buildNumber} ${title.trim()} (<${url}|Open>)"
+  if(message) result = result + "\nChanges:\n\t ${message.trim()}"
+  if(artifact) result = result + "\nArtifacts:\n\t ${artifact.trim()}"
+  if(testSummary) result = result + "\n ${testSummary}"
+
+  return result
+}
